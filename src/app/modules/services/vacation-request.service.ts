575c224d4ad3ecb4d10e9383fcabc1056967f2fb
justEliada <eliadaballazhi@gmail.com>
2025-02-24 14:54:47 +0100

    adding vacation request for substitution

--- src/app/modules/services/vacation-request.service.ts
+++ src/app/modules/services/vacation-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { VacationRequest } from 'src/app/core/models/vacationRequest.model';
+import { VacationResponse } from 'src/app/core/models/VacationResponse.model';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,27 @@ export class VacationRequestService {
     return this.http.get(`${this.baseUrl}/${id}/get-least-created`);
   }
 
+  getReplacementRequests(replacementUserId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/replacement-requests/${replacementUserId}`);
+  }
+
+  getPendingRequests(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/pending-requests/${userId}`);
+  }
+
+  approveReplacementUser(timeSheetId: number, replacementUserId: number): Observable<any> {
+    return this.http.post(`${this.baseUrl}/approve-replacement/${timeSheetId}/${replacementUserId}`, {});
+  }
+
+  rejectReplacementUser(timeSheetId: number, replacementUserId: number): Observable<any> {
+    return this.http.post(`${this.baseUrl}/reject-replacement/${timeSheetId}/${replacementUserId}`, {});
+  }
+
+  //TO DO
+  getAllTimeSheetsForReplacementUser(replacementUserId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/replacement-user/${replacementUserId}`);
+  }
+
   getAllVacationRequestsById(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/${id}/all`);
   }
@@ -28,10 +50,19 @@ export class VacationRequestService {
     return this.http.get(`${this.baseUrl}/all`);
   }
 
+  getApprovedVactionsList(userID: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${userID}/approved`);
+  }
+
   approveUserRequest(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/approve/${id}`);
   }
   rejectUserRequest(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/reject/${id}`);
   }
+
+  getAllRequestsForReplacementUser(replacementUserId: number): Observable<VacationResponse[]> {
+    return this.http.get<VacationResponse[]>(`${this.baseUrl}/replacement-requests/${replacementUserId}/get-all`);
+  }
+  
 }
